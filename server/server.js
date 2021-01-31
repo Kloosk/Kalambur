37376eaefc3472f62f3220f32354cb9158c43e6e
Kloosk <lukasz.gras.97@gmail.com>
2021-01-31 19:28:18 +0100

    Config socket

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+const cors = require('cors');
+
+//Cors
+app.use(cors());
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on("mouse", data => {
+        socket.broadcast.emit("mouse", data);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(4000, () => {
+    console.log('listening on *:4000');
+});
